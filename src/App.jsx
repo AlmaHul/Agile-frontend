b1e4e48679975f30f8e93f47dacce92e2f028f09
David Abdulkarim <david.abdulkarim@hotmail.com>
2025-10-01 11:31:45 +0200

    ändrling logga in

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import RegisterPage from "./pages/RegisterPage";
 function App() {
   return (
     <Routes>
-      <Route path="/login" element={<LoginPage />} />
+      <Route path="/" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
       <Route
         path="/home"
