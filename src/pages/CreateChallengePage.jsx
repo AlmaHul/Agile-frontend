137e616b018bcabba1701094465cb6dcdcef1671
Abbekf <albin.k.forsberg@gmail.com>
2025-10-07 20:34:15 +0200

    added a basic CreateChallengePage structure

---
+++ src/pages/CreateChallengePage.jsx
@@ -0,0 +1,9 @@
+
+export default function CreateChallengePage() {
+return (
+    <div style={{ padding: "2rem" }}>
+    <h1>Create Challenge Page</h1>
+    <p>This is where you’ll later create a new challenge.</p>
+    </div>
+);
+}
