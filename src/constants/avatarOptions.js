c1c23da934b82a36ef4e499cbca9859f72b348d9
AlmaHul <almaarriazahult@gmail.com>
2025-10-08 10:47:45 +0200

    Fil med features för avatar

---
+++ src/constants/avatarOptions.js
@@ -0,0 +1,51 @@
+// src/constants/avatarOptions.js
+
+export const hairOptions = [
+  "NoHair", "ShortHairShortFlat", "ShortHairShortRound", "ShortHairShortWaved",
+  "ShortHairSides", "ShortHairTheCaesar", "ShortHairTheCaesarSidePart",
+  "LongHairBigHair", "LongHairBob", "LongHairBun", "LongHairCurly", "LongHairCurvy",
+  "LongHairDreads", "LongHairFrida", "LongHairFro", "LongHairFroBand", "LongHairNotTooLong",
+  "LongHairShavedSides", "LongHairMiaWallace", "LongHairStraight", "LongHairStraight2",
+  "LongHairStraightStrand"
+];
+
+export const accessoriesOptions = [
+  "Blank", "Kurt", "Prescription01", "Prescription02", "Round", "Sunglasses", "Wayfarers"
+];
+
+export const hairColorOptions = [
+  "Auburn", "Black", "Blonde", "BlondeGolden", "Brown", "BrownDark", "PastelPink", "Platinum",
+  "Red", "SilverGray"
+];
+
+export const clotheOptions = [
+  "BlazerShirt", "BlazerSweater", "CollarSweater", "GraphicShirt", "Hoodie",
+  "Overall", "ShirtCrewNeck", "ShirtScoopNeck", "ShirtVNeck"
+];
+
+export const clotheColorOptions = [
+  "Black", "Blue01", "Blue02", "Blue03", "Gray01", "Gray02", "Heather", "PastelBlue",
+  "PastelGreen", "PastelOrange", "PastelRed", "PastelYellow", "Pink", "Red", "White"
+];
+
+export const eyeOptions = [
+  "Close", "Cry", "Default", "Dizzy", "EyeRoll", "Happy", "Hearts", "Side", "Squint", "Surprised", "Wink", "WinkWacky"
+];
+
+export const eyebrowOptions = [
+  "Angry", "AngryNatural", "Default", "DefaultNatural", "FlatNatural",
+  "RaisedExcited", "RaisedExcitedNatural", "SadConcerned", "SadConcernedNatural", "UnibrowNatural", "UpDown", "UpDownNatural"
+];
+
+export const mouthOptions = [
+  "Concerned", "Default", "Disbelief", "Eating", "Grimace", "Sad", "Screaming", "Serious", "Smile", "Tongue", "Twinkle", "Vomit"
+];
+
+export const skinOptions = [
+  "Tanned", "Yellow", "Pale", "Light", "Brown", "DarkBrown", "Black"
+];
+
+// Facial hair options
+export const facialHairOptions = [
+  "Blank", "BeardMedium","BeardLight","BeardMajestic","MoustacheFancy","MoustacheMagnum"
+];
