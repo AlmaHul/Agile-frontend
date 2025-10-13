aa8a1eb86ff2e5128b6a0b83ec5407f4cf22c7c1
David Abdulkarim <david.abdulkarim@hotmail.com>
2025-10-13 12:54:11 +0200

    man måste nu vara inloggat för att komma på vissa sidor

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import LandingPage from "./pages/LandingPage";
-import LoginPage from "./pages/LoginPage";  // Denna importeras
+import LoginPage from "./pages/LoginPage";
 import HomePage from "./pages/HomePage";
 import ProtectedRoute from "./components/ProtectedRoute";
-import RegisterPage from "./pages/RegisterPage";  // Denna importeras
+import RegisterPage from "./pages/RegisterPage";
 import CreateAvatarPage from "./pages/CreateAvatarPage";
 import UpdateAvatarPage from "./pages/UpdateAvatarPage";
 import CreateChallengePage from "./pages/CreateChallengePage";
@@ -16,15 +16,45 @@ function App() {
     <>
       <Header /> {/* Header syns på alla sidor */}
       <Routes>
+        {/* Öppna sidor */}
         <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<LoginPage />} />  {/* Ändra till LoginPage */}
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/reset-password/:token" element={<ResetPassword />} />
-        <Route path="/register" element={<RegisterPage />} />  {/* Ändra till RegisterPage */}
-        <Route path="/create-avatar" element={<CreateAvatarPage />} />
-        <Route path="/update-avatar" element={<UpdateAvatarPage />} />
-        <Route path="/create-challenge" element={<CreateChallengePage />} />
 
+        {/* Skyddade sidor – kräver inloggning */}
+        <Route
+          path="/register"
+          element={
+            <ProtectedRoute>
+              <RegisterPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/create-avatar"
+          element={
+            <ProtectedRoute>
+              <CreateAvatarPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/update-avatar"
+          element={
+            <ProtectedRoute>
+              <UpdateAvatarPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/create-challenge"
+          element={
+            <ProtectedRoute>
+              <CreateChallengePage />
+            </ProtectedRoute>
+          }
+        />
         <Route
           path="/profile"
           element={
@@ -33,6 +63,8 @@ function App() {
             </ProtectedRoute>
           }
         />
+
+        {/* 404-sida */}
         <Route path="*" element={<h1>404 Not Found</h1>} />
       </Routes>
     </>
