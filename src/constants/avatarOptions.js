74f9c789cdc9e3d73da25df8f8ee1486d33db3cb
AlmaHul <almaarriazahult@gmail.com>
2025-10-28 13:00:43 +0000

    Översätt api options till svenska labels

--- src/constants/avatarOptions.js
+++ src/constants/avatarOptions.js
@@ -1,5 +1,6 @@
 // src/constants/avatarOptions.js
 
+// 1️⃣ Hårtyper
 export const hairOptions = [
   "NoHair", "ShortHairShortFlat", "ShortHairShortRound", "ShortHairShortWaved",
   "ShortHairSides", "ShortHairTheCaesar", "ShortHairTheCaesarSidePart",
@@ -9,43 +10,163 @@ export const hairOptions = [
   "LongHairStraightStrand"
 ];
 
-export const accessoriesOptions = [
-  "Blank", "Kurt", "Prescription01", "Prescription02", "Round", "Sunglasses", "Wayfarers"
-];
+export const hairLabels = {
+  NoHair: "Ingen hår",
+  ShortHairShortFlat: "Kort platt",
+  ShortHairShortRound: "Kort rund",
+  ShortHairShortWaved: "Kort vågig",
+  ShortHairSides: "Kort sidor",
+  ShortHairTheCaesar: "Caesar",
+  ShortHairTheCaesarSidePart: "Caesar sidodel",
+  LongHairBigHair: "Stort hår",
+  LongHairBob: "Bob",
+  LongHairBun: "Knut",
+  LongHairCurly: "Lockigt",
+  LongHairCurvy: "Vågigt",
+  LongHairDreads: "Dreadlocks",
+  LongHairFrida: "Frida",
+  LongHairFro: "Afro",
+  LongHairFroBand: "Afro med band",
+  LongHairNotTooLong: "Inte för långt",
+  LongHairShavedSides: "Rakat på sidor",
+  LongHairMiaWallace: "Mia Wallace",
+  LongHairStraight: "Rakt hår",
+  LongHairStraight2: "Rakt hår 2",
+  LongHairStraightStrand: "Rakt slingor"
+};
 
-export const hairColorOptions = [
-  "Auburn", "Black", "Blonde", "BlondeGolden", "Brown", "BrownDark", "PastelPink", "Platinum",
-  "Red", "SilverGray"
-];
+// 2️⃣ Accessoarer
+export const accessoriesOptions = ["Blank", "Kurt", "Prescription01", "Prescription02", "Round", "Sunglasses", "Wayfarers"];
+export const accessoriesLabels = {
+  Blank: "Ingen",
+  Kurt: "Kurt",
+  Prescription01: "Glasögon 01",
+  Prescription02: "Glasögon 02",
+  Round: "Runda",
+  Sunglasses: "Solglasögon",
+  Wayfarers: "Wayfarers"
+};
 
-export const clotheOptions = [
-  "BlazerShirt", "BlazerSweater", "CollarSweater", "GraphicShirt", "Hoodie",
-  "Overall", "ShirtCrewNeck", "ShirtScoopNeck", "ShirtVNeck"
-];
+// 3️⃣ Hårfärger
+export const hairColorOptions = ["Auburn", "Black", "Blonde", "BlondeGolden", "Brown", "BrownDark", "PastelPink", "Platinum", "Red", "SilverGray"];
+export const hairColorLabels = {
+  Auburn: "Rödbrun",
+  Black: "Svart",
+  Blonde: "Blond",
+  BlondeGolden: "Gyllene blond",
+  Brown: "Brun",
+  BrownDark: "Mörkbrun",
+  PastelPink: "Pastellrosa",
+  Platinum: "Platina",
+  Red: "Röd",
+  SilverGray: "Silvergrå"
+};
 
-export const clotheColorOptions = [
-  "Black", "Blue01", "Blue02", "Blue03", "Gray01", "Gray02", "Heather", "PastelBlue",
-  "PastelGreen", "PastelOrange", "PastelRed", "PastelYellow", "Pink", "Red", "White"
-];
+// 4️⃣ Kläder
+export const clotheOptions = ["BlazerShirt", "BlazerSweater", "CollarSweater", "GraphicShirt", "Hoodie", "Overall", "ShirtCrewNeck", "ShirtScoopNeck", "ShirtVNeck"];
+export const clotheLabels = {
+  BlazerShirt: "Kavaj & skjorta",
+  BlazerSweater: "Kavaj & tröja",
+  CollarSweater: "Tröja med krage",
+  GraphicShirt: "T-shirt med tryck",
+  Hoodie: "Hoodie",
+  Overall: "Overall",
+  ShirtCrewNeck: "T-shirt rund hals",
+  ShirtScoopNeck: "T-shirt scoop-hals",
+  ShirtVNeck: "T-shirt V-hals"
+};
 
-export const eyeOptions = [
-  "Close", "Cry", "Default", "Dizzy", "EyeRoll", "Happy", "Hearts", "Side", "Squint", "Surprised", "Wink", "WinkWacky"
-];
+// 5️⃣ Färg på kläder
+export const clotheColorOptions = ["Black", "Blue01", "Blue02", "Blue03", "Gray01", "Gray02", "Heather", "PastelBlue", "PastelGreen", "PastelOrange", "PastelRed", "PastelYellow", "Pink", "Red", "White"];
+export const clotheColorLabels = {
+  Black: "Svart",
+  Blue01: "Blå 1",
+  Blue02: "Blå 2",
+  Blue03: "Blå 3",
+  Gray01: "Grå 1",
+  Gray02: "Grå 2",
+  Heather: "Heather",
+  PastelBlue: "Pastellblå",
+  PastelGreen: "Pastellgrön",
+  PastelOrange: "Pastellorange",
+  PastelRed: "Pastellröd",
+  PastelYellow: "Pastellgul",
+  Pink: "Rosa",
+  Red: "Röd",
+  White: "Vit"
+};
 
-export const eyebrowOptions = [
-  "Angry", "AngryNatural", "Default", "DefaultNatural", "FlatNatural",
-  "RaisedExcited", "RaisedExcitedNatural", "SadConcerned", "SadConcernedNatural", "UnibrowNatural", "UpDown", "UpDownNatural"
-];
+// 6️⃣ Ögon
+export const eyeOptions = ["Close", "Cry", "Default", "Dizzy", "EyeRoll", "Happy", "Hearts", "Side", "Squint", "Surprised", "Wink", "WinkWacky"];
+export const eyeLabels = {
+  Close: "Stängda",
+  Cry: "Gråt",
+  Default: "Standard",
+  Dizzy: "Yr",
+  EyeRoll: "Ögonen uppåt",
+  Happy: "Glad",
+  Hearts: "Hjärta",
+  Side: "Sida",
+  Squint: "Squint",
+  Surprised: "Överraskad",
+  Wink: "Blink",
+  WinkWacky: "Galet blink"
+};
 
-export const mouthOptions = [
-  "Concerned", "Default", "Disbelief", "Eating", "Grimace", "Sad", "Screaming", "Serious", "Smile", "Tongue", "Twinkle", "Vomit"
-];
+// 7️⃣ Ögonbryn
+export const eyebrowOptions = ["Angry", "AngryNatural", "Default", "DefaultNatural", "FlatNatural", "RaisedExcited", "RaisedExcitedNatural", "SadConcerned", "SadConcernedNatural", "UnibrowNatural", "UpDown", "UpDownNatural"];
+export const eyebrowLabels = {
+  Angry: "Arg",
+  AngryNatural: "Arg naturlig",
+  Default: "Standard",
+  DefaultNatural: "Standard naturlig",
+  FlatNatural: "Platta naturlig",
+  RaisedExcited: "Uppspärrade",
+  RaisedExcitedNatural: "Uppspärrade naturlig",
+  SadConcerned: "Bekymrad",
+  SadConcernedNatural: "Bekymrad naturlig",
+  UnibrowNatural: "Enbryn",
+  UpDown: "Upp & ner",
+  UpDownNatural: "Upp & ner naturlig"
+};
 
-export const skinOptions = [
-  "Tanned", "Yellow", "Pale", "Light", "Brown", "DarkBrown", "Black"
-];
+// 8️⃣ Mun
+export const mouthOptions = ["Concerned", "Default", "Disbelief", "Eating", "Grimace", "Sad", "Screaming", "Serious", "Smile", "Tongue", "Twinkle", "Vomit"];
+export const mouthLabels = {
+  Concerned: "Bekymrad",
+  Default: "Standard",
+  Disbelief: "Misstro",
+  Eating: "Äter",
+  Grimace: "Grimas",
+  Sad: "Ledsen",
+  Screaming: "Skriker",
+  Serious: "Allvarlig",
+  Smile: "Leende",
+  Tongue: "Tunga",
+  Twinkle: "Gnista",
+  Vomit: "Kräks"
+};
 
-// Facial hair options
-export const facialHairOptions = [
-  "Blank", "BeardMedium","BeardLight","BeardMajestic","MoustacheFancy","MoustacheMagnum"
-];
+// 9️⃣ Hudfärg
+export const skinOptions = ["Tanned", "Yellow", "Pale", "Light", "Brown", "DarkBrown", "Black"];
+export const skinLabels = {
+  Tanned: "Solbränd",
+  Yellow: "Gul",
+  Pale: "Blekt",
+  Light: "Ljus",
+  Brown: "Brun",
+  DarkBrown: "Mörkbrun",
+  Black: "Svart"
+};
+
+// 🔟 Skägg/mustasch
+export const facialHairOptions = ["Blank", "BeardMedium","BeardLight","BeardMajestic","MoustacheFancy","MoustacheMagnum"];
+export const facialHairLabels = {
+  Blank: "Ingen",
+  BeardMedium: "Medium skägg",
+  BeardLight: "Ljust skägg",
+  BeardMajestic: "Majestätiskt skägg",
+  MoustacheFancy: "Fancy mustasch",
+  MoustacheMagnum: "Magnum mustasch"
+};
+
