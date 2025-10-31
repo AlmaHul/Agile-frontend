fda31c24fbb0a68822f17e8664e28e72ed1ad664
AlmaHul <almaarriazahult@gmail.com>
2025-10-31 10:52:00 +0000

    Ändra render API

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,5 @@
 const LOCAL_URL = "http://127.0.0.1:5000/api/";
-const PROD_URL = "https://agile-project-4.onrender.com/api/";
+const PROD_URL = "https://jora-backend.onrender.com/api/";
 
   // Välj automatiskt beroende på miljö (process.env.NODE_ENV)
 export const API_URL =
