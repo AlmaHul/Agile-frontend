00fcea470cd0fe1843862b6128f22d76302c87cb
David Abdulkarim <david.abdulkarim@hotmail.com>
2025-11-03 13:53:39 +0100

    Fixade score så man kan se på profil sida

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,6 +4,3 @@ const PROD_URL = "https://agile-project-4.onrender.com/api/";
   // Välj automatiskt beroende på miljö (process.env.NODE_ENV)
 export const API_URL =
   process.env.NODE_ENV === "production" ? PROD_URL : LOCAL_URL;
-
-
-  //https://jora-backend.onrender.com/api/
